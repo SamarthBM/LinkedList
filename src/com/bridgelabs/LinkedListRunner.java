90d988ae5dad663977dcedd7d2dcadfa94f77591
SamarthBM <samarthbm18@gmail.com>
2021-07-15 19:16:30 +0530

    Initial commit

---
+++ src/com/bridgelabs/LinkedListRunner.java
@@ -0,0 +1,15 @@
+/****************************************************************
+ * Purpose : 
+ * @author Samarth BM
+
+***************************************************************/
+
+package com.bridgelabs;
+
+public class LinkedListRunner {
+
+	public static void main(String[] args) {
+
+	}
+
+}
