942f46ecd65017aebf036031ba45d3cafc85ac33
SamarthBM <samarthbm18@gmail.com>
2021-07-15 23:18:15 +0530

    Added: created simple linked list of 56, 30 and 70

--- src/com/bridgelabs/LinkedListRunner.java
+++ src/com/bridgelabs/LinkedListRunner.java
@@ -1,5 +1,5 @@
 /****************************************************************
- * Purpose : 
+ * Purpose : Create a simple Linked List of 56, 30 and 70.
  * @author Samarth BM
 
 ***************************************************************/
@@ -9,6 +9,19 @@ package com.bridgelabs;
 public class LinkedListRunner {
 
 	public static void main(String[] args) {
+		MyNode<Integer> myFirstNode = new MyNode<>(56);
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(70);
+		
+		// Linking the nodes.
+		myFirstNode.setNext(mySecondNode);
+		mySecondNode.setNext(myThirdNode);
+		
+		// Checking whether nodes are linked successfully.
+		if (myFirstNode.getNext().equals(mySecondNode) && mySecondNode.getNext().equals(myThirdNode))
+			System.out.println("Linked successfully");
+		else
+			System.out.println("Linking unsuccessfull");
 
 	}
 
